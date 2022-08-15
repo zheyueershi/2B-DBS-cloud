fad6df7243272e4b8c8dbc9c41d1a78a4a488f47
zheyueershi <111163173+zheyueershi@users.noreply.github.com>
2022-08-15 14:36:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from flask import Flask, render_template, request
 
-
-# In[2]:
-
-
 app = Flask(__name__)
-
-
-# In[3]:
-
 
 import joblib
 
@@ -33,57 +19,9 @@ def index():
     else:
         return(render_template("index.html",result1="waiting",result2="waiting",result3="waiting"))
 
-
-# In[ ]:
-
-
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=int("5002"))
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
